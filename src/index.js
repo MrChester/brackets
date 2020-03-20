7fcc94d01b53d0744a7855c852ad78853e198c44
Yauheni Ramankou <iromankov2010@gmail.com>
2020-03-20 18:59:43 +0300

    feat: implement brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var strArr = str.split(''),
+        stack = [],
+        iter = 0;
+
+    if (str.length % 2 != 0) {
+        return false;
+    }
+
+    while (iter < str.length) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            if (strArr[iter] === bracketsConfig[i][1] && stack[stack.length - 1] === bracketsConfig[i][0]) {
+                stack.pop();
+            } else
+            if (strArr[iter] === bracketsConfig[i][0]) {
+                stack.push(strArr[iter]);
+            }
+        }
+        iter++;
+    }
+    if (stack.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
